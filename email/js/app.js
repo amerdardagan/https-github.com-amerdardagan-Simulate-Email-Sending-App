d8480209bcd962dba37a83f0fb6a20fa48dfe8bf
amer dardagan <amigo_gagi@outlook.com>
2019-09-08 13:13:14 +0200

    added event listeners for fields validation

--- email/js/app.js
+++ email/js/app.js
@@ -1,5 +1,8 @@
 //variables
-const sendBtn = document.getElementById ('sendBtn');
+const sendBtn = document.getElementById ('sendBtn'),
+       email = document.getElementById ('email'),
+       subject = document.getElementById ('subject')
+       message = document.getElementById ('message');
 
 //event listeners
 eventListeners ();
@@ -7,18 +10,32 @@ eventListeners ();
 function eventListeners () {
 
 //App Init - inits gunction when page loads
-document.addEventListener ('DOMContentLoaded', appInit)
+document.addEventListener ('DOMContentLoaded', appInit);
+
+//Valideate the form
+email.addEventListener ('blur' , validateField); //blur:napisati nešto u polje pa click u sljedeće
+subject.addEventListener ('blur' , validateField);
+message.addEventListener ('blur' , validateField);
+
 
 }
 
 //functions 
     //App Init (kada prvi put loadiram stranicu 
         //1.želim da SEND bude disable
-        //2.da ispunim 2 validate polja prije nego mogu SEND
+        //2.da ispunim 3 validate polja prije nego mogu SEND
     function appInit () {
         //disable SEND button on load
         sendBtn.disabled = true;
 
+    }
+
+    //Validate the Fields
+    function validateField () {
+        let errors;
+
+        //Validate the length of the field
+        validateLength (this); //odnosi se na trenutno polje koje kliknimo
     }
 
 
